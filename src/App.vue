aa96b9a3b4e05c7422b8a70811034780dfa805a0
Daria Burdyga <Daria.Burdyga@t-systems.com>
2021-12-21 15:34:01 +0300

    Add style to table

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
     <h2>Streaming market data</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>ISIN</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="stock in stocks" :key="stock.isin">
-          <th scope="row">1</th>
-          <td>{{ stock.isin }}</td>
-          <td>{{ stock.price }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>ISIN</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="stock in stocks" :key="stock.isin">
+            <th scope="row">1</th>
+            <td>{{ stock.isin }}</td>
+            <td>{{ stock.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
   <router-view />
 </template>
@@ -29,7 +31,7 @@ import store from "@/store";
 import { GET_STOCKS } from "@/store/modules/stocks";
 
 export default defineComponent({
-  name: "Home",
+  name: "App",
   created() {
     websocketService.init("ws://159.89.15.214:8080/");
   },
@@ -50,9 +52,35 @@ export default defineComponent({
   letter-spacing: unset;
   font-weight: 500;
 }
+html,
+body {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  font-family: DINNextLTPro, sans-serif;
+  font-weight: 100;
+}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 td,
 th {
-  border: 1px solid grey;
+  padding: 15px;
+  color: black;
+}
+
+th {
+  text-align: left;
+}
+
+thead {
+  background-color: #36df77;
 }
 
 #nav {
