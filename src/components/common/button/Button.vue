abc1a6c4a82c6c248fd78e101deeade55884f9da
Daria Burdyga <Daria.Burdyga@t-systems.com>
2021-12-22 23:41:57 +0300

    Button component

--- src/components/common/button/Button.vue
+++ src/components/common/button/Button.vue
@@ -1,13 +1,25 @@
 <template>
-$END$
+  <button :id="id" :type="type">
+    <slot />
+  </button>
 </template>
 
-<script>
-export default {
-name: "Button"
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: () => "button",
+    },
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
